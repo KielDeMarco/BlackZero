bfdcb116c1cfe175e7307257db3a0e80f8acbb21
KielDeMarco <130366465+KielDeMarco@users.noreply.github.com>
2024-06-25 14:22:30 -0600

    Update bloodhound.sh

--- Linux/Bash_Scripts/bloodhound.sh
+++ Linux/Bash_Scripts/bloodhound.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+#Requires docker to be installed.
+
 xhost + && sudo docker run -it --rm -v $(pwd)/bloodhound:/bloodhound  -e DISPLAY=$DISPLAY --network host --device /dev/dri/card0 --name bloodhound bannsec/bloodhound --cpus="2" --oom-kill-disable --memory="4096"
